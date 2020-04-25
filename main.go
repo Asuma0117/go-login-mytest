7c5c4e85eb004626b84630bb30d5487efcb8108f
Asuma0117 <asuma.rain711@gmail.com>
2020-04-25 18:13:17 +0900

    test

--- main.go
+++ main.go
@@ -1,60 +1,22 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// )
-
-// func main() {
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		log.Fatal("$PORT must be set")
-// 	}
-
-// 	http.HandleFunc("/", handler)
-// 	http.ListenAndServe(":"+port, nil)
-// }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
-// }
-
 package main
+
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"github.com/labstack/echo/v4"
+	"os"
 )
-type User struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
+
 func main() {
-	// サーバー用のインスタンスの取得
-	e := echo.New()
-	// ユーザー
-	u := User{
-		Email:    "me@example.com",
-		Password: "password",
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
 	}
-	// ルーティング設定
-	e.GET("/helloworld", helloWorld)
-	e.POST("/login", func(c echo.Context) error {
-		r := new(User)
-		if err := c.Bind(r); err != nil {
-			return c.String(http.StatusBadRequest, err.Error())
-		}
-		if r.Email != u.Email || r.Password != u.Password {
-			return c.String(http.StatusUnauthorized, "login fail")
-		}
-		// 暫定
-		token := "sampletoken"
-		return c.String(http.StatusOK, "{\"token\":\""+token+"\"}")
-	})
-	// サーバー起動
-	e.Logger.Fatal(e.Start(":1323"))
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+port, nil)
 }
-func helloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "hello world!!")
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
 }
